4ddafb64f653daf35294fd348d97b2ab9fa59b92
ipiyumii <sendtopiyumi@gmail.com>
2024-07-01 22:59:09 +0530

    last commit maybe

--- src/app/tasks/new-task/new-task.component.ts
+++ src/app/tasks/new-task/new-task.component.ts
@@ -1,17 +1,39 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { NewTaskData } from './../task/task.model';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Title } from '@angular/platform-browser';
+import { TaskService } from '../tasks.services';
 
 @Component({
   selector: 'app-new-task',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './new-task.component.html',
   styleUrl: './new-task.component.css'
 })
 export class NewTaskComponent {
-  isStopAddingTask = false;
-  @Output()stop = new EventEmitter<boolean>()
+  @Input({required: true}) userId!: string
+  @Output()stop = new EventEmitter<void>()
+  // @Output()stop = new EventEmitter<void>()
+
+  constructor(private taskService: TaskService) {}
+
+  enteredTitle = '';
+  enteredSummary = '';
+  enteredDate = '';
 
   onStopAddTask() {
-    this.stop.emit(this.isStopAddingTask)
+    this.stop.emit()
+  }
+
+  onSubmit() {
+    this.taskService.addTask({
+      title: this.enteredTitle,
+      summary: this.enteredSummary,
+      date: this.enteredDate
+    },
+    this.userId )
+
+    this.stop.emit()
   }
 }
--- src/app/tasks/task/task.component.ts
+++ src/app/tasks/task/task.component.ts
@@ -1,5 +1,8 @@
+import { TaskService } from './../tasks.services';
 import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { DatePipe } from '@angular/common';
 import { Task } from './task.model';
+import { CardComponent } from '../../shared/card/card.component';
 
 // interface Task {
 //   id: string;
@@ -12,15 +15,19 @@ import { Task } from './task.model';
 @Component({
   selector: 'app-task',
   standalone: true,
-  imports: [],
+  imports: [CardComponent, DatePipe],
   templateUrl: './task.component.html',
   styleUrl: './task.component.css'
 })
+
 export class TaskComponent {
   @Input({required: true}) task!: Task;
   @Output() complete = new EventEmitter<string>();
 
+  constructor(private taskService: TaskService) {}
+
   onCompleteTask() {
-    this.complete.emit(this.task.id)
+    // this.complete.emit(this.task.id)
+    this.taskService.removeTask(this.task.id)
   }
 }
